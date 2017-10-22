311071ad4d08696d388d142d85fa847875ade8d2
stevew <steve@digipulse.io>
2017-10-22 23:10:48 +0200

    sip local

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const { Environment } = require('storj');
 
 // SIP6
 const storj = new Environment({
-  bridgeUrl: 'https://alpha.digipulse.io:8080',
+  bridgeUrl: 'http://localhost:8080',
   bridgeUser: 'steve@digipulse.io',
   bridgePass: 'test',
   encryptionKey: 'test',
