86ccca5d6fba5b0060208e774a0a3cc00056f9e6
brusselscoder <steve@digipulse.io>
2017-10-27 00:17:12 -0700

    Update

--- routes/dev.js
+++ routes/dev.js
@@ -1,10 +1,9 @@
 /* DIGIPULSE ALPHA SIP5 + SIP6 IMPLENTATION
-* This code combines SIP5 + SIP6 and will be known as SIP7
-*
-* DO NOT FORGET TO SET A NEW SESSION_KEY BEFORE LIVE STAGE !!!
-* Code by Steve Walschot
-*/
-
+ * This code combines SIP5 + SIP6 and will be known as SIP7
+ *
+ * DO NOT FORGET TO SET A NEW SESSION_KEY BEFORE LIVE STAGE !!!
+ * Code by Steve Walschot
+ */
 var express = require('express');
 var router = express.Router();
 
@@ -15,7 +14,7 @@ var crypto = require('crypto');
 
 /* DEVELOPMENT SWITCH;
  * Use only local filesystem when true, DGP network when false
-*/
+ */
 var isDev = false;
 
 // SIP5
@@ -26,282 +25,299 @@ var client;
 var keypair;
 
 // SIP6
-const { Environment } = require('storj');
+const {
+  Environment
+} = require('storj');
 var storj;
 
 /* MULTER UPLOAD TESTING
  * Should improve images
-*/
+ */
 var multer = require('multer');
 
 const tmpDir = '/temp';
 
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function(req, file, cb) {
     var randomFolder = genRandomString(24);
-    var tmpPath = '/home/steve/dgp-larva-sip6'  + tmpDir + '/' + randomFolder;
+    var tmpPath = '/home/steve/dgp-larva-sip6' + tmpDir + '/' + randomFolder;
     fs.mkdirsSync(tmpPath)
     cb(null, tmpPath)
   },
   filename: function(req, file, callback) {
-		console.log(file)
-		callback(null, file.originalname)
-	}
+    console.log(file)
+    callback(null, file.originalname)
+  }
 });
 
-var upload = multer({ storage: storage });
+var upload = multer({
+  storage: storage
+});
 
 // END multer
 
-function LoginSIP5 (user, pass) {
+function LoginSIP5(user, pass) {
   return false;
 }
 
 function storeSessionKey(req, key, user) {
-    req.session.authed = true;
-    req.session.keypair = key;
-    req.session.email = user.email;
-    req.session.password = encrypt(SESSION_KEY, user.password);
+  req.session.authed = true;
+  req.session.keypair = key;
+  req.session.email = user.email;
+  req.session.password = encrypt(SESSION_KEY, user.password);
 }
 
 function encrypt(key, data) {
-    var cipher = crypto.createCipher('aes-256-cbc', key);
-    var crypted = cipher.update(data, 'utf-8', 'hex');
-    crypted += cipher.final('hex');
+  var cipher = crypto.createCipher('aes-256-cbc', key);
+  var crypted = cipher.update(data, 'utf-8', 'hex');
+  crypted += cipher.final('hex');
 
-    return crypted;
+  return crypted;
 }
 
 function decrypt(key, data) {
-    var decipher = crypto.createDecipher('aes-256-cbc', key);
-    var decrypted = decipher.update(data, 'hex', 'utf-8');
-    decrypted += decipher.final('utf-8');
+  var decipher = crypto.createDecipher('aes-256-cbc', key);
+  var decrypted = decipher.update(data, 'hex', 'utf-8');
+  decrypted += decipher.final('utf-8');
 
-    return decrypted;
+  return decrypted;
 }
 
-var genRandomString = function(length){
-    return crypto.randomBytes(Math.ceil(length/2))
-        .toString('hex') /** convert to hexadecimal format */
-        .slice(0,length);   /** return required number of characters */
+var genRandomString = function(length) {
+  return crypto.randomBytes(Math.ceil(length / 2))
+    .toString('hex') /** convert to hexadecimal format */
+    .slice(0, length); /** return required number of characters */
 };
 
 var auth = function(req, res, next) {
-    if (req.session && req.session.authed)
-        return next();
-    else
-        console.log('Session NOT found');
-        return res.render('dev/login');
-        //return res.send({ status: 'fail', message: 'not logged in to DigiPulse network' });
+  if (req.session && req.session.authed)
+    return next();
+  else
+    console.log('Session NOT found');
+  return res.render('dev/login');
+  //return res.send({ status: 'fail', message: 'not logged in to DigiPulse network' });
 };
 
 /* LOGOUT */
-router.get('/logout',   function(req, res, next) {
+router.get('/logout', function(req, res, next) {
   req.session.destroy(function(err) {
-    return res.send({ status: 'success' });
+    return res.send({
+      status: 'success'
+    });
   });
 });
 
 
-router.post('/login', function(req, res, next) {
-
-  /* This is the only stage where the password is needed in plaintext.
-  *  Once sent to the sessios, it get's encrypted by the SESSION_KEY.
-  */
-
-  // SIP5 basic login
-  var user = {email: req.body.user, password: req.body.pass};
-  console.log(user);
-
-
-  var client = storjlib.BridgeClient(DIGIPULSE_HUB, {basicAuth: user});
-  var keypair = storjlib.KeyPair();
-
-  client.addPublicKey(keypair.getPublicKey(), function(err) {
-    if (err) {
-      req.session.destroy();
-      return res.send({ status: 'fail', message: err.message });
-    }
-    storeSessionKey(req, keypair.getPrivateKey(), user);
-
-    // Login to SIP6 after success from SIP5
-    // SIP 6 authed and loading vaults
-    storj = new Environment({
-     bridgeUrl: DIGIPULSE_HUB,
-     bridgeUser: req.session.email,
-     bridgePass: decrypt(SESSION_KEY, req.session.password),
-     encryptionKey: 'test',
-     logLevel: 4
-   });
-
-   storj.getBuckets(function(err, result) {
-     if (err) {
-       req.session.destroy();
-       return res.send({  status: 'fail', message: err.message});
-     }
-     // UX return
-     //return res.render('index', {result: result});
-     // PLAIN TEST RETURN
-     return res.send({ status: 'success', message: 'Login success' });
-   });
-  });
-});
 
 /* GET GENERAL INFO */
 router.get('/', auth, function(req, res, next) {
 
-    return res.render('dev/index');
-    //return res.send({ result: result });
+  return res.render('dev/index');
+  //return res.send({ result: result });
 
 });
 
-/* LIST ALL VAULTS */
-router.get('/vault', auth, function(req, res, next) {
-
-  storj = new Environment({
-   bridgeUrl: DIGIPULSE_HUB,
-   bridgeUser: req.session.email,
-   bridgePass: decrypt(SESSION_KEY, req.session.password),
-   encryptionKey: 'test',
-   logLevel: 4
- });
-
-  storj.getBuckets(function(err, result) {
-    if (err) {
-      return res.send({ status: 'fail', message: err.message });
-    }
-    return res.send({ status: 'success', result: result });
-  });
-});
 
 /* CREATE VAULTS */
 router.get('/vault/create/:name', auth, function(req, res, next) {
   var vaultName = req.params.name;
 
   storj = new Environment({
-   bridgeUrl: DIGIPULSE_HUB,
-   bridgeUser: req.session.email,
-   bridgePass: decrypt(SESSION_KEY, req.session.password),
-   encryptionKey: 'test',
-   logLevel: 4
- });
+    bridgeUrl: DIGIPULSE_HUB,
+    bridgeUser: req.session.email,
+    bridgePass: decrypt(SESSION_KEY, req.session.password),
+    encryptionKey: 'test',
+    logLevel: 4
+  });
 
   storj.createBucket(vaultName, function(err, result) {
     if (err) {
       return console.error(err);
     }
-    return res.send({ result: result });
+    return res.send({
+      result: result
+    });
     storj.destroy();
   });
 });
 
-/* LIST ALL FILES */
+
+/* Login with user:pass
+ * @dev: Working with AJAX.
+*/
+router.post('/login', function(req, res, next) {
+
+  /* This is the only stage where the password is needed in plaintext.
+   *  Once sent to the sessios, it get's encrypted by the SESSION_KEY.
+   */
+
+  // SIP5 basic login
+  var user = {
+    email: req.body.user,
+    password: req.body.pass
+  };
+  console.log(user);
+
+
+  var client = storjlib.BridgeClient(DIGIPULSE_HUB, {
+    basicAuth: user
+  });
+  var keypair = storjlib.KeyPair();
+
+  client.addPublicKey(keypair.getPublicKey(), function(err) {
+    if (err) {
+      req.session.destroy();
+      return res.send({
+        status: 'fail',
+        message: err.message
+      });
+    }
+    storeSessionKey(req, keypair.getPrivateKey(), user);
+
+    // Login to SIP6 after success from SIP5
+    // SIP 6 authed and loading vaults
+    storj = new Environment({
+      bridgeUrl: DIGIPULSE_HUB,
+      bridgeUser: req.session.email,
+      bridgePass: decrypt(SESSION_KEY, req.session.password),
+      encryptionKey: 'test',
+      logLevel: 4
+    });
+
+    storj.getBuckets(function(err, result) {
+      if (err) {
+        req.session.destroy();
+        return res.send({
+          status: 'fail',
+          message: err.message
+        });
+      }
+      // UX return
+      //return res.render('index', {result: result});
+      // PLAIN TEST RETURN
+      return res.send({
+        status: 'success',
+        message: 'Login success'
+      });
+    });
+  });
+});
+
+
+/* List all vaults
+ * @dev: Working with AJAX.
+*/
+router.get('/vault', auth, function(req, res, next) {
+
+  storj = new Environment({
+    bridgeUrl: DIGIPULSE_HUB,
+    bridgeUser: req.session.email,
+    bridgePass: decrypt(SESSION_KEY, req.session.password),
+    encryptionKey: 'test',
+    logLevel: 4
+  });
+
+  storj.getBuckets(function(err, result) {
+    if (err) {
+      return res.send({
+        status: 'fail',
+        message: err.message
+      });
+    }
+    return res.send({
+      status: 'success',
+      result: result
+    });
+  });
+});
+
+
+/* List all files in drive
+ * @dev: Working with AJAX.
+*/
 router.post('/vault', auth, function(req, res, next) {
 
   var vaultName = req.body.driveID;
 
   storj = new Environment({
-   bridgeUrl: DIGIPULSE_HUB,
-   bridgeUser: req.session.email,
-   bridgePass: decrypt(SESSION_KEY, req.session.password),
-   encryptionKey: 'test',
-   logLevel: 4
- });
+    bridgeUrl: DIGIPULSE_HUB,
+    bridgeUser: req.session.email,
+    bridgePass: decrypt(SESSION_KEY, req.session.password),
+    encryptionKey: 'test',
+    logLevel: 4
+  });
 
   storj.listFiles(vaultName, function(err, result) {
     if (err) {
-      return res.send({ status: 'fail', message: err.message });
+      return res.send({
+        status: 'fail',
+        message: err.message
+      });
     }
-    return res.send({status: 'success', result: result });
+    return res.send({
+      status: 'success',
+      result: result
+    });
     //storj.destroy();
   });
 });
 
-/* Upload files */
-router.get('/vault/file/upload/:vault/:filepath', auth, function(req, res, next) {
-  const bucketId = 'ed45fc44067d75a558e45785';
 
+/* Upload files
+ * @dev: Working with AJAX.
+ * @info Current code does NOT support multiple files. Loop will be required if needed.
+ * Advice: using XHR we can track progress update in browser
+*/
+router.post('/vault/upload', auth, upload.any(), function(req, res, next) {
 
-
-  // File to upload
-  const uploadFilePath = '/home/steve/dgp-larva-sip6/package.json';
-  // file stored as
-  const fileName = 'README1.json';
-
-  storj = new Environment({
-   bridgeUrl: DIGIPULSE_HUB,
-   bridgeUser: req.session.email,
-   bridgePass: decrypt(SESSION_KEY, req.session.password),
-   encryptionKey: 'test',
-   logLevel: 4
- });
-
-  storj.storeFile(bucketId, uploadFilePath, {
-  filename: fileName,
-  progressCallback: function(progress, uploadedBytes, totalBytes) {
-    console.log('Progress: %d, uploadedBytes: %d, totalBytes: %d',
-                progress, uploadedBytes, totalBytes);
-  },
-  finishedCallback: function(err, fileId) {
-    if (err) {
-      return console.error(err);
-    }
-    return res.send({ result: fileId });
-    }
-  });
-});
-
-/* Upload files */
-router.post('/vault/upload', auth,  upload.any(), function(req, res, next) {
-
-  if(isDev) {
-    console.log(req.body); //form fields
-  	// example output:{ vaultID: '000000000000' }
-  	console.log(req.files); //form files
-  	// example output:{ fieldname: 'upl',originalname: 'grumpy.png',encoding: '7bit',mimetype: 'image/png',destination: './uploads/',filename: '436ec561793aa4dc475a88e84776b1b9',path: 'uploads/436ec561793aa4dc475a88e84776b1b9',size: 277056 }
-    return res.send({ status: 'success', result: req.files });
+  if (isDev) {
+    console.log(req.body);
+    // example output:{ vaultID: '000000000000' }
+    console.log(req.files); //form files
+    // example output:{ fieldname: 'upl',originalname: 'grumpy.png',encoding: '7bit',mimetype: 'image/png',destination: './uploads/',filename: '436ec561793aa4dc475a88e84776b1b9',path: 'uploads/436ec561793aa4dc475a88e84776b1b9',size: 277056 }
+    return res.send({
+      status: 'success',
+      result: req.files
+    });
     //res.status(204).end();
   } else {
 
+    var bucketId = req.body.driveID;
     var files = req.files;
-    if(files){
-        files.forEach(function(file){
-          console.log('-----------------------------------------');
-          console.log(file.path);
-          console.log('-----------------------------------------');
 
-          var bucketId = req.body.driveID;
-          // File to upload
-          var uploadFilePath = file.path;
-          // file stored on vault as
-          var fileName = file.filename;
+    if (files) {
+      files.forEach(function(file) {
+        console.log('-----------------------------------------');
+        console.log(file);
+        console.log('-----------------------------------------');
 
-          console.log(bucketId);
-          console.log(uploadFilePath);
-          console.log(fileName);
+        var uploadFilePath = file.path;
+        var fileName = file.filename;
 
-          storj = new Environment({
-           bridgeUrl: DIGIPULSE_HUB,
-           bridgeUser: req.session.email,
-           bridgePass: decrypt(SESSION_KEY, req.session.password),
-           encryptionKey: 'test',
-           logLevel: 4
-          });
+        storj = new Environment({
+          bridgeUrl: DIGIPULSE_HUB,
+          bridgeUser: req.session.email,
+          bridgePass: decrypt(SESSION_KEY, req.session.password),
+          encryptionKey: 'test',
+          logLevel: 4
+        });
 
-          storj.storeFile(bucketId, uploadFilePath, {
+        storj.storeFile(bucketId, uploadFilePath, {
           filename: fileName,
           progressCallback: function(progress, uploadedBytes, totalBytes) {
             console.log('Progress: %d, uploadedBytes: %d, totalBytes: %d',
-                        progress, uploadedBytes, totalBytes);
+              progress, uploadedBytes, totalBytes);
           },
           finishedCallback: function(err, fileId) {
             if (err) {
-              return console.error(err);
+              return res.send({status: 'fail',message: err.message});
             }
-            return res.send({ status: 'success', result: fileId });
-            }
-          });
+            // Remove file stored on system
+            fs.removeSync(file.destination );
+            return res.send({status: 'success',result: fileId});
+          }
         });
+      });
     }
 
 
@@ -310,40 +326,80 @@ router.post('/vault/upload', auth,  upload.any(), function(req, res, next) {
 
 });
 
-router.get('/vault/file/download/:vault/:fileid', auth, function(req, res, next) {
+
+/* Download files
+ * @dev: Working with AJAX.
+*/
+router.post('/vault/download', auth, function(req, res, next) {
 
 
-  var bucketId = req.params.vault;
-  var fileId = req.params.fileid;
-  var downloadFilePath = './test.js';
+  try {
+      var randomFolder = genRandomString(24);
+      var tmpPath = '/home/steve/dgp-larva-sip6'  + tmpDir + '/' + randomFolder;
+      console.log(tmpPath);
+      if (!fs.existsSync(tmpPath)){
+            fs.mkdirSync(tmpPath);
+        }
 
-  storj = new Environment({
-   bridgeUrl: DIGIPULSE_HUB,
-   bridgeUser: req.session.email,
-   bridgePass: decrypt(SESSION_KEY, req.session.password),
-   encryptionKey: 'test',
-   logLevel: 4
- });
 
-  storj.resolveFile(bucketId, fileId, downloadFilePath, {
-    progressCallback: function(progress, downloadedBytes, totalBytes) {
-      console.log('Progress: %d, downloadedBytes: %d, totalBytes: %d',
-                  progress, downloadedBytes, totalBytes);
-    },
-    finishedCallback: function(err) {
-      if (err) {
-        return console.error(err);
+    var bucketId = req.body.driveID;
+    var fileId = req.body.fileID;
+    var downloadFilePath = tmpPath + '/' + req.body.fileNAME;
+
+    console.log('init download' + downloadFilePath);
+  } catch (err) {
+    console.log (err);
+  }
+
+    storj = new Environment({
+     bridgeUrl: DIGIPULSE_HUB,
+     bridgeUser: req.session.email,
+     bridgePass: decrypt(SESSION_KEY, req.session.password),
+     encryptionKey: 'test',
+     logLevel: 4
+   });
+
+    storj.resolveFile(bucketId, fileId, downloadFilePath, {
+      progressCallback: function(progress, downloadedBytes, totalBytes) {
+        console.log('Progress: %d, downloadedBytes: %d, totalBytes: %d',
+                    progress, downloadedBytes, totalBytes);
+      },
+      finishedCallback: function(err) {
+        if (err) {
+          return res.send({ status: 'fail', message: err.message });
+        }
+
+        //res.download(downloadFilePath);
+        //fsextra.removeSync(tmpPath);
+        return res.send({ status: 'success', message: 'download complete', tmp:randomFolder  });
+
+        //return res.send({ result: 'fileId' });
+        //storj.destroy();
       }
-      console.log('File download complete');
-      //return res.send({ result: 'fileId' });
-      storj.destroy();
-    }
-  });
+    });
+
 
 });
 
 
+/* Download file to client browser
+ * @dev: Working with AJAX.
+ * Advice: beautify
+*/
+router.get('/download/:tmp/:file', function(req, res, next) {
 
+    var fileNAME = '/home/steve/dgp-larva-sip6/temp/' + req.params.tmp + '/' +req.params.file;
+
+    res.download(fileNAME, function(err) {
+      if (err) {
+      // Handle error, but keep in mind the response may be partially-sent
+      // so check res.headersSent
+    } else {
+      fs.removeSync('/home/steve/dgp-larva-sip6/temp/' + req.params.tmp );
+    }
+  });
+
+});
 
 module.exports = router;
 
